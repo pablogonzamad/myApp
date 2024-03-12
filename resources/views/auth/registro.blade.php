1ba512bdd4b6990d19f9a6c06816364b38aee182
pablogonzamad <pablogomad@email.com>
2024-03-12 17:43:28 -0300

    .

--- resources/views/auth/registro.blade.php
+++ resources/views/auth/registro.blade.php
@@ -12,7 +12,7 @@
 
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
             <form action="/registro" method="POST">
-                @csr
+                @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
                         Nombre
